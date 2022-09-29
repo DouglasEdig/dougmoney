df3534fb747239e4efdab5854cfd8389e271ac78
douglasEdig <douglas.edig@outlook.com>
2022-09-29 00:18:21 -0300

    modal

--- src/Styles/global.ts
+++ src/Styles/global.ts
@@ -75,4 +75,16 @@ export const GlobalStyles = createGlobalStyle`
     border-radius:0.24rem ;
   }
 
+  .modal-close{
+    position: absolute;
+    right: 1.5rem;
+    top:1.5rem;
+    border: 0;
+    background: transparent;
+    transition: filter 0.2s;
+    &:hover{
+      filter:brightness(0.8)
+    }
+  }
+
 `;
--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from "react-modal";
-import { Container } from "./styles";
-
+import { Container, ButtonTransaction } from "./styles";
+import closeImg from "../../assets/close.svg";
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
 interface NewTransationModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
@@ -17,10 +19,23 @@ export const NewTranslationModal = ({
       overlayClassName="react-modal-overlay"
       className="react-modal-content"
     >
+      <button type="button" onClick={onRequestClose} className="modal-close">
+        <img src={closeImg} alt="close" />
+      </button>
       <Container>
         <h2>Cadastrar transação</h2>
         <input placeholder="Titulo" />
-        <input type="number" placeholder="Titulo" />
+        <input type="number" placeholder="Valor" />
+        <ButtonTransaction>
+          <button type="button">
+            <img src={incomeImg} alt="entradas" />
+            <span>Entradas</span>
+          </button>
+          <button type="button">
+            <img src={outcomeImg} alt="saidas" />
+            <span>Saídas</span>
+          </button>
+        </ButtonTransaction>
         <input placeholder="Categoria" />
         <button type="submit">Cadastrar</button>
       </Container>
